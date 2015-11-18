d9f7e777de859123dea9f74232223ef3ec64ff96
benswift404 <swiftmac234@gmail.com>
2015-11-17 19:48:10 -0500

    Created Guild rails project and added some styling

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GuildPrototype::Application.config.secret_key_base = '866e55612a20165b702f6caa2b4ee4b32c0ebb332ed2499469f5247e2461f5c3906db960a4aad4401c12dc2543e79f1bbb5fc4b3045bc3dc30a40069ccbb9684'
